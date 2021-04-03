efc4bf5d534e9e59a549b0543ed94b70a9a9424d
hetvishukla2001 <64137184+hetvishukla2001@users.noreply.github.com>
2021-04-03 15:53:43 +0530

    First.java

---
+++ First.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class First {
+
+	public static void main(String[] args) {
+
+		int a,b,c;
+		
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("enter no1:");
+		a=scan.nextInt();
+		System.out.println("enter no2:");// TODO Auto-generated method stub
+
+		b=scan.nextInt();
+		c=a+b;
+		System.out.println("\n add : "+c);
+		
+		
+	}
+
+}
